93c3e90191ad06cc789830c007989946304e270e
MinaAshraf-Developer <mina.ashraf2024@gmail.com>
2025-03-07 16:15:24 +0200

    Initial commit

---
+++ Task one.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Task_one
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your favorite number between 1 and 100: ");
+           string input = Console.ReadLine();
+
+            Console.WriteLine("Amazing!! That's my favorite number too!");
+            Console.WriteLine($"No really!!, {input} is my favorite number!");
+
+
+        }
+    }
+}
