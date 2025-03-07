733c738a730572402ff8b9d056874d223ea807e6
MinaAshraf-Developer <mina.ashraf2024@gmail.com>
2025-03-07 16:47:14 +0200

    Initial commit

--- Task one.cs
+++ Task one.cs
@@ -7,11 +7,29 @@ namespace Task_one
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your favorite number between 1 and 100: ");
-           string input = Console.ReadLine();
+            Console.Write("What is the number of small carpets?: ");
+           int smallCarpet = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Amazing!! That's my favorite number too!");
-            Console.WriteLine($"No really!!, {input} is my favorite number!");
+            Console.Write("What is the number of large carpets?: ");
+            int largeCarpet = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Price per small room: $25");
+            Console.WriteLine("Price per large room: $35");
+
+            int cost = (smallCarpet * 25 + largeCarpet * 35);
+            double tax = cost * 0.06;
+
+            Console.WriteLine($"Cost : {cost}");
+            Console.WriteLine($"Tax : {tax}");
+
+            Console.WriteLine("========================");
+            Console.WriteLine($"Total Estimat: {cost + tax}");
+            Console.WriteLine($"This estimate is valid for 30 days");
+
+
+
+
+         
 
 
         }
